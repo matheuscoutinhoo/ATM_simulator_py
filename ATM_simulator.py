f6271c5ef8f372dad88945ceecb35aed3f42ed12
coringa net <matheusfcneto@gmail.com>
2022-02-22 11:54:48 -0300

    ATM Simulator

---
+++ ATM_simulator.py
@@ -0,0 +1,65 @@
+'''
+A program that simulates an ATM with the following options:
+1 – Deposit money (deposit of any amount)
+2 - Check balance
+3 – Withdraw money (only if you have a balance)
+4 –Exit (The program only ends if the user enters the option)
+
+'''
+
+
+opt= int(input('Hello. How can we help you?\n[1] - Deposit\n[2] - Check balance\n[3] - Withdraw\n[4] - exit '))
+balance = 0
+
+while True:
+    if opt == 1:
+        dep = float(input('How many do you want to deposit: '))
+        balance = balance + dep
+        print(f'Your balance now is R$ {balance:.2f}')
+        repeater = (input('Do you want to continue? (y/n) ')).lower()
+        repeater.strip()
+        if repeater == 'y':
+            opt = int(input('Hello. How can we help you?\n[1] - Deposit\n[2] - Check balance\n[3] - Withdraw\n[4] - exit '))
+        else:
+            print('Ok. Thanks.')
+            break
+
+    elif opt == 2:
+        print(f'Your balance right now is R$ {balance}')
+        if balance == 0:
+            print('You do not have any balance. Please make a deposit.')
+        repeater = (input('Do you want to continue? (y/n) ')).lower()
+        repeater.strip()
+        if repeater == 'y':
+            opt = int(input('Hello. How can we help you?\n[1] - Deposit\n[2] - Check balance\n[3] - Withdraw\n[4] - exit '))
+        else:
+            print('Ok. Thanks.')
+            break
+
+    elif opt == 3:
+        if balance == 0:
+            print('You do not have any balance. Please make a deposit.')
+            repeater = (input('Do you want to continue? (y/n) ')).lower()
+            repeater.strip()
+            if repeater == 'y':
+                opt = int(input('Hello. How can we help you?\n[1] - Deposit\n[2] - Check balance\n[3] - Withdraw\n[4] - exit '))
+            else:
+                print('Ok. Thanks.')
+                break
+        else:
+            withdraw = int(input('How many do you want to withdraw: '))
+            balance = balance - withdraw
+            print(f'You have withdrawed R$ {withdraw}.\nYour balance now is R$ {balance}')
+            repeater = (input('Do you want to continue? (y/n) ')).lower()
+            repeater.strip()
+            if repeater == 'y':
+                opt = int(input('Hello. How can we help you?\n[1] - Deposit\n[2] - Check balance\n[3] - Withdraw\n[4] - exit '))
+            else:
+                print('Ok. Thanks.')
+                break
+
+    elif opt == 4:
+        print('Thank you')
+        break
+
+
